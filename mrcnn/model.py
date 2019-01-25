5af4d7707423adfb1f88f2a1d21579e8f9e982cd
pub773 <45876891+pub773@users.noreply.github.com>
2019-01-25 15:27:31 +0100

    disabled multiprocessing

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2371,8 +2371,8 @@ class MaskRCNN():
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
-            workers=workers,
-            use_multiprocessing=True,
+            workers=0, 				# disable multiprocessing
+            use_multiprocessing=False,
         )
         self.epoch = max(self.epoch, epochs)
 
